a052b1e044b047cecdadb51946c1eba5c90a89ff
Carlos <paulonluis222@gmail.com>
2025-12-04 20:48:29 -0300

    config: update security configuration for JWT

--- src/main/java/com/carlos/todoapi/config/SecurityConfig.java
+++ src/main/java/com/carlos/todoapi/config/SecurityConfig.java
@@ -1,30 +1,75 @@
 package com.carlos.todoapi.config;
 
+import com.carlos.todoapi.security.CustomUserDetailsService;
+import com.carlos.todoapi.security.JwtAuthenticatorFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+    private final JwtAuthenticatorFilter jwtAuthenticatorFilter; //maybe final
+    private final CustomUserDetailsService userDetailsService;
+
+    public SecurityConfig(JwtAuthenticatorFilter jwtAuthenticatorFilter, CustomUserDetailsService userDetailsService) {
+        this.jwtAuthenticatorFilter = jwtAuthenticatorFilter;
+        this.userDetailsService = userDetailsService;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(); //visualizar pois está deprecated
+        authProvider.setUserDetailsService(userDetailsService); //deprecated
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
     // Permite tudo entre requisições
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)  // Desabilita CSRF (para testes)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()  // Permite TODAS as requisições sem autenticação
-                );
+        http.csrf(AbstractHttpConfigurer::disable)// Desabilita CSRF (para testes)
+
+                //Stateless session - jwt não usa session
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                //rotas + login e register públicos e resto protegido
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+
+                //provider
+                .authenticationProvider(authenticationProvider())
+
+                //Filtro JWT ante do filtro default de login
+                .addFilterBefore(jwtAuthenticatorFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
+
 }
