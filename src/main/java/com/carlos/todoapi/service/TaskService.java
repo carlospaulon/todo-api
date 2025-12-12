b97ba502625c3d397c70b787387dca0db6cf7867
Carlos <paulonluis222@gmail.com>
2025-12-12 11:07:31 -0300

    feat: implement full/partial update, delete and status-based filtering methods for TaskService

--- src/main/java/com/carlos/todoapi/service/TaskService.java
+++ src/main/java/com/carlos/todoapi/service/TaskService.java
@@ -1,8 +1,10 @@
 package com.carlos.todoapi.service;
 
 import com.carlos.todoapi.dto.request.CreateTaskRequest;
+import com.carlos.todoapi.dto.request.UpdateTaskRequest;
 import com.carlos.todoapi.dto.response.TaskResponse;
 import com.carlos.todoapi.entity.Task;
+import com.carlos.todoapi.entity.TaskStatus;
 import com.carlos.todoapi.entity.User;
 import com.carlos.todoapi.exception.TaskNotFoundException;
 import com.carlos.todoapi.exception.UnauthorizedAccessException;
@@ -56,17 +58,98 @@ public class TaskService {
 
     }
 
-    //Trocar a exceção
     public TaskResponse getTaskById(Long id, String username) {
-        Task taskById= taskRepository.findById(id)
+        Task taskById = taskRepository.findById(id)
                 .orElseThrow(() -> new TaskNotFoundException("Task by ID not found"));
 
-        //se o usuário não for o mesmo, lança exceção
         if (!taskById.getUser().getUsername().equals(username)) {
             throw new UnauthorizedAccessException("Acesso não autorizado");
         }
 
         return taskMapper.toResponse(taskById);
 
+    }
+
+    //Method update - watch out with repetive code
+    public TaskResponse updateTask(Long id, UpdateTaskRequest request, String username) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException("Task by ID not found"));
+
+        if (!task.getUser().getUsername().equals(username)) {
+            throw new UnauthorizedAccessException("Acesso não autorizado");
+        }
+
+        if (request.title() != null) {
+            task.setTitle(request.title());
+        }
+
+        if (request.description() != null) {
+            task.setDescription(request.description());
+        }
+
+        if (request.status() != null) {
+            task.setStatus(request.status());
+        }
+
+        if (request.priority() != null) {
+            task.setPriority(request.priority());
+        }
+
+        if (request.dueDate() != null) {
+            task.setDueDate(request.dueDate());
+        }
+
+        Task taskSaved = taskRepository.save(task);
+
+        return taskMapper.toResponse(taskSaved);
+
+    }
+
+    public TaskResponse updateTaskStatus(Long id, TaskStatus status, String username) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException("Task by ID not found"));
+
+        if (!task.getUser().getUsername().equals(username)) {
+            throw new UnauthorizedAccessException("Acesso não autorizado");
+        }
+
+        //not necessary verify if it's null?
+        task.setStatus(status);
+
+        Task taskSaved = taskRepository.save(task);
+
+        return taskMapper.toResponse(taskSaved);
+
+    }
+
+    public void deleteTask(Long id, String username) {
+
+        //method to not repeat code?
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException("Task by ID not found"));
+
+        if (!task.getUser().getUsername().equals(username)) {
+            throw new UnauthorizedAccessException("Acesso não autorizado");
+        }
+
+        taskRepository.delete(task); //ou por id, entender qual o melhor?
+
+    }
+
+    public List<TaskResponse> getTasksByUserAndStatus(String username, TaskStatus status) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        List<Task> tasks = null;
+
+        if (status == null) {
+            tasks = taskRepository.findByUserId(user.getId());
+        } else {
+            tasks = taskRepository.findByUserIdAndStatus(user.getId(), status);
+        }
+
+        return tasks.stream()
+                .map(taskMapper::toResponse)
+                .collect(Collectors.toList());
     }
 }
