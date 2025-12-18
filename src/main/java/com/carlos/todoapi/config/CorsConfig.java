7f65103f59beebff47949b8a7f2cf25e778b1b7b
Carlos <paulonluis222@gmail.com>
2025-12-18 16:27:37 -0300

    config: add CORS configuration file

---
+++ src/main/java/com/carlos/todoapi/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.carlos.todoapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                //use env variables
+                                "http://localhost:3000", //react
+                                "http://localhost:4200", //angular
+                                "http://localhost:5173"  //vite
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
